018582e07331bb97b3e54299463b0d7638ca4455
Muskan Mittal <mittalmuskan758@gmail.com>
2024-03-06 17:46:02 +0530

    updated code

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,11 @@
-const Footer  = () => {
+const Footer = () => {
     return (
-        <footer>
-
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                    &copy; 2023 FakeStoreNameA, Inc. All rights reserved.
+                </p>
+            </div>
         </footer>
     )
 }
